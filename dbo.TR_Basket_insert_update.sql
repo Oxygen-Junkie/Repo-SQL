9d53f19245f252f4c01c0932916197c4242fd170
John Doe <johndoe@email.com>
2023-08-19 22:35:54 +0400

    Triggers

---
+++ dbo.TR_Basket_insert_update.sql
@@ -0,0 +1,15 @@
+ CREATE TRIGGER Basket_insert_update ON dbo.Basket
+    AFTER INSERT AS
+    BEGIN
+      DECLARE @DiscountValue AS decimal(18, 2);
+      SET @DiscountValue = 0;
+
+      IF (1 < (SELECT count(*) FROM dbo.Basket WHERE ID_SKU = inserted.ID_SKU AND PurchaseDate = GETDATE())) 
+        BEGIN
+          SET @DiscountValue = CAST(inserted.Value * 5 as decimal(18, 2)) / 100;
+        END;
+
+      UPDATE dbo.Basket
+      SET DiscountValue = 0
+      WHERE ID = inserted.ID;
+    END;
